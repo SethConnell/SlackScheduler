cdbae5060e503d55c556676c1761494f37140840
Seth J. Connell <sethjordanconnell@gmail.com>
2019-07-02 18:42:20 -0500

    Fixed time function.

--- appfunctions.py
+++ appfunctions.py
@@ -1,13 +1,16 @@
-
+from datetime import datetime
+import pytz
 def convertGivenTimeToEpoch(year, month, day, hour, minute, ampm):
-    from datetime import *
     m2 = str(hour) + ":" + str(minute) + " " + str(ampm.lower())
-    m2 = datetime.strptime(m2, '%I:%M %p')
+    m2 = datetime.datetime.strptime(m2, '%I:%M %p')
     m2 = str(m2)
     m2 = m2.split(" ")[1]
-    m2.split(":")
+    m2 = m2.split(":")
+    m2[0] = m2[0].replace(":", "")
+    m2[1] = m2[1].replace(":", "")
+    m2[2] = m2[2].replace(":", "")
+    print(m2[0] + ":" + m2[1] + ":" + m2[2])
+    time = datetime.datetime(int(year), int(month), int(day), int(m2[0]), int(m2[1]), int(m2[2])).timestamp()
+    return str(time)
+#Usage: convertGivenTimeToEpoch("2019", "6", "26", "11", "00", "pm")
 
-    time2 = datetime.datetime(1970,1,1,0,0,0)
-    time1 = datetime.datetime(year, month, day, m2[0], m2[1], m2[2])
-    x = (time1 - time2).total_seconds()
-    return x
